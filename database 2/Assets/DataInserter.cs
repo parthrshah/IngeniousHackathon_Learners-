d6386c20ec083a25e8734b3a6e983f521db4b52a
parthrshah <parth05shah@gmail.com>
2018-03-03 20:56:23 +0530

    commit 4

---
+++ database 2/Assets/DataInserter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataInserter : MonoBehaviour
+{
+
+    public string inputUserName;
+    public string inputPassword;
+    public string inputEmail;
+
+    string CreateUserURL = "http://localhost/Cool/InsertUser.php";
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) CreateUser(inputUserName, inputPassword, inputEmail);
+    }
+
+    public void CreateUser(string username, string password, string email)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("usernamePost", username);
+        form.AddField("passwordPost", password);
+        form.AddField("emailPost", email);
+
+        WWW www = new WWW(CreateUserURL, form);
+    }
+}
